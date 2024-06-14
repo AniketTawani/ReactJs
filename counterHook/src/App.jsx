a24835ce7b66522211620a7c97cb00e0c57b66a6
Aniket Tawani <anikettawani17@gmail.com>
2024-06-14 21:03:05 +0530

    Learn one interview question

--- counterHook/src/App.jsx
+++ counterHook/src/App.jsx
@@ -7,7 +7,24 @@ function App() {
   let [count, setCount] = useState(0)
 
   const addValue = () => {
-    setCount(count+1);
+    // setCount(count+1);
+    // setCount(count+1);
+    // setCount(count+1);
+    // setCount(count+1);
+    // setCount(count+1);
+    // setCount(count+1);
+    // setCount(count+1);
+
+
+    //Interiew question
+    //basically setCount function supports call back function, with the help of this 
+    //we can track the state of the previously/latest updation of the variable.
+    setCount(prevCount=>prevCount+1)
+    setCount(prevCount=>prevCount+1)
+    setCount(prevCount=>prevCount+1)
+    setCount(prevCount=>prevCount+1)
+    setCount(prevCount=>prevCount+1)
+
   }
 
   const removeValue = () =>{
