607712ba653078df2c70d3917bdd8fabb19e4705
Aniket Tawani <anikettawani17@gmail.com>
2024-06-18 09:10:38 +0530

    learn about the useState, useCallback, useRef, useEffect hook

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -8,7 +8,9 @@ function App() {
       style={{backgroundColor:color}}
     >
       <div className='fixed flex flex-wrap justify-center bottom-12 px-2 inset-x-0'>
+        {/* <h1>Test</h1> */}
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'>
+        {/* <h1>Test</h1> */}
           <button className='outline-none rounded-full px-4 shadow-lg text-white' style={{backgroundColor:"red"}} onClick={()=>setColor('red')}>Red</button>
           <button className='outline-none rounded-full px-4 shadow-lg text-white' style={{backgroundColor:"green"}} onClick={()=>setColor('green')}>Green</button>
           <button className='outline-none rounded-full px-4 shadow-lg text-white' style={{backgroundColor:"olive"}} onClick={()=>setColor('olive')}>Olive</button>
